16774b352a0a42120e2e984fa0016819bacf4acc
Rohit Sharma <rohit10012@gmail.com>
2022-06-20 14:43:46 +0530

    Guessing game

--- printVowel.py
+++ printVowel.py
@@ -1,4 +1,5 @@
 #print file without vowels
+'''
 text = input("Enter a string: ")
 i = 0
 while i < len(text):
@@ -6,3 +7,18 @@ while i < len(text):
     if ch not in "aeiouAEIOU":
         print(ch, end="")
     i += 1
+'''
+
+#method 2 for this same program using continue
+
+text = input("Enter a string: ")
+i = 0
+while i < len(text):
+    ch = text[i]
+    i += 1
+    if ch in "aeiouAEIOU":
+        continue
+    print(ch, end="")
+
+
+
