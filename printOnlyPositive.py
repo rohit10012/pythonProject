8cb00dccb443291b160118fab920fdcca9b6e508
Rohit Sharma <rohit10012@gmail.com>
2022-06-20 14:51:28 +0530

    Guessing game

---
+++ printOnlyPositive.py
@@ -0,0 +1,12 @@
+sum = 0
+while True:
+    n = int(input("Enter the number"))
+    if n == 0:
+        break
+    elif n < 0:
+        continue
+    else:
+        sum = sum + n
+        continue
+print(sum)
+
---
+++ sumAllTillZero.py
@@ -0,0 +1,10 @@
+sum = 0
+while True:
+    n = int(input("Enter the number"))
+    if n == 0:
+        break
+    else:
+        sum = sum + n
+        continue
+print(sum)
+
